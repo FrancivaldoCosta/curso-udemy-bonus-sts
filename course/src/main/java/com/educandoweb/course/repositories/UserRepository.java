e22253e754976d2a259e36512dff000bf75d77db
Francivaldo Costa <francivaldoaccosta@gmail.com>
2024-11-17 09:05:03 -0300

    Service layer, component registration

--- course/src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ course/src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoweb.course.entities.User;
 
+//@Respsitory "NÃO É NESCESSARIO" TA ERDANDO DO JPA REPOSITORY QUE JA ESTA REGISTRADO COMO UM COMPLEMENTO SPRING
 //NESSE CASO ESPECIFICO NÃO PRECISA CRIAR A IMPLEMENTAÇÃO DA INTERFACE, O JPA JA TEM IMPLEMENTAÇÃO PADRAO DE INTERFACE
 public interface UserRepository extends JpaRepository<User, Long> {
 
