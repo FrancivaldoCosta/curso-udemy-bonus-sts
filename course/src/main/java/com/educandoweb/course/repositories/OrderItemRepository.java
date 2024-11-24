6f3d0cfd0b715e37b3e3a40a89af41dd7df42165
Francivaldo Costa <francivaldoaccosta@gmail.com>
2024-11-24 01:30:14 -0300

    User insert

--- course/src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
+++ course/src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.educandoweb.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoweb.course.entities.OrderItem;
+import com.educandoweb.course.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long/*aqui estava Long, mais tava dando erro e o copilot sugeriu alterar pra OrderItemPK e corrigiu*/>  {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> /*(Inserir Long)aqui estava Long, mais tava dando erro e o copilot sugeriu alterar pra OrderItemPK e corrigiu*/ {
 
 }
