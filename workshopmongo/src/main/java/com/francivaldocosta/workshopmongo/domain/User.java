b83154eba56b1fbc3af87a6736debbbea216b276
Francivaldo Costa <francivaldoaccosta@gmail.com>
2025-01-31 08:30:55 -0300

    Referenciando os posts do usuario

--- workshopmongo/src/main/java/com/francivaldocosta/workshopmongo/domain/User.java
+++ workshopmongo/src/main/java/com/francivaldocosta/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.francivaldocosta.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user") // Se colocar somente o @document funciona, porem o spring vai pegaar o mesmo nome da classe, so que em letra minuscula.
@@ -14,6 +17,9 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true) // lazy é pra carregar os dados do usuario somente quando eu chamar os posts pra não sobrecarregar.
+	private List<Post> posts = new ArrayList<>(); // Lembrando que no Java a lista é apenas uma INTERFACE e o ArrayList<> é uma implementação possível desta lista.
 
 	public User() {
 	}
@@ -48,6 +54,14 @@ public class User implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
 
 	@Override
 	public int hashCode() {
@@ -65,5 +79,7 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 
 }
